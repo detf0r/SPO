8d5e427aeed46ba82884c3b3466b832ee500a85e
detf0r <ekimenkoip@gmail.com>
2023-10-10 17:29:01 +0700

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -14,32 +14,9 @@ vector<int> read_array(fstream &in, int n){
     }
     return array;
 }
-
-pair<int,int>find_min_and_max(vector<int> &array){
-    int min = array[0];
-    int max = array[0];
-    for (int i : array){
-        if (i<min)
-            min = i;
-        if (i>max)
-            max = i;
-    }
-    return make_pair(min,max);
-}
-
-int find_min_pos(vector <int> &array, int min){
-    for (int pos = 0; pos < array.size(); pos++){
-        if (array[pos]== min){
-            return pos;
-        }
-    }
-    return 0;
-}
-
 vector<int> find_nums_between_minmax(vector<int> &sortedArray, pair <int,int> minMax){
-    int pos = find_min_pos(sortedArray,minMax.first);
     vector<int> numsBtwMinMax;
-    for (int i = pos; (i < sortedArray.size()-1) && (sortedArray[i] < minMax.second);i++){
+    for (int i = 0; (i < sortedArray.size()-1) && (sortedArray[i] < minMax.second);i++){
         for (int j = sortedArray[i]+1; (j < sortedArray[i+1]) && (j < minMax.second); j++){
             numsBtwMinMax.push_back(j);
         }
@@ -63,8 +40,9 @@ int main(){
     int n;
     input >> n;
     vector<int> array = read_array(input,n);
-    pair<int,int> minMax = find_min_and_max(array);
+    
     sort(array.begin(), array.end());
+    pair<int,int> minMax(array[0],array[array.size()-1]);
     vector<int> numsBetwMinMax = find_nums_between_minmax(array,minMax);
     print_nums(numsBetwMinMax);
     print_nums(array);
