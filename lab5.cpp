581e8d1084529015d233bfc113ccaaeb666632eb
detf0r <ekimenkoip@gmail.com>
2023-10-10 21:07:09 +0700

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -21,43 +21,66 @@ vector<int> read_array(fstream &in, int n){
     }
     return array;
 }
-vector<int> find_nums_between_minmax(vector<int> &sortedArray, pair <int,int> minMax){
+int * find_nums_between_minmax(vector<int> &sortedArray, pair <int,int> minMax){
     vector<int> numsBtwMinMax;
+    numsBtwMinMax.resize(minMax.second - minMax.first);
+
     int semid = semget (IPC_PRIVATE,1,IPC_CREAT|0666);
     int shmid = shmget(IPC_PRIVATE,2,IPC_CREAT|0666);
+    int shmid1 = shmget(IPC_PRIVATE,2*(minMax.second - minMax.first),IPC_CREAT|0666);
     if (semid < 0 || shmid < 0){
         cout << "Ошибка создания семафора...";
-        return numsBtwMinMax;
+        return nullptr;
     }
 
 
     sembuf Plus1 = {0, 1, 0};
     sembuf Minus1 = {0, -1, 0};
-    sembuf MinusNumOfProc = {0, -NumOfProc, 0};
+    //sembuf MinusNumOfProc = {0, -NumOfProc, 0};
 
     semop(semid,&Plus1,1);
-    auto mem = (vector<int>*)shmat(shmid,NULL,0);
-
+    int *k = (int*)shmat(shmid,NULL,0);
+    int *arr = (int*)shmat(shmid1,NULL,0);
+    //vector<int> numsBtwMinMax1
     int intervalForProc = sortedArray.size() / NumOfProc;
-    
+    *k = 0;
+
     for (int proc = 0; proc < NumOfProc; proc++){
         if (fork()==0){
             for (int i = proc*intervalForProc; (i < proc*intervalForProc+intervalForProc) && (i < sortedArray.size()-1) && (sortedArray[i] < minMax.second); i++){
-                semop(semid,&Plus1,1);
+                
                 for (int j = sortedArray[i]+1; (j < sortedArray[i+1]) && (j < minMax.second); j++){
-                    numsBtwMinMax.push_back(j);
+                    semop(semid,&Minus1,1);
+
+                    numsBtwMinMax[*k] = j; 
+                    arr[*k] = j;
+                    *k = *k+1;
+                    //cout << j << " ";
+                    
+                    semop(semid,&Plus1,1);
                 }
             }
+            exit(0);
         }
     }
-
+    cout << endl;
+    //system("ipcs -s");
+    string temp = "ipcs -s -i " + to_string(semid);
+    system(temp.c_str());
+    wait(NULL);
     
-    for (int i = 0; (i < sortedArray.size()-1) && (sortedArray[i] < minMax.second);i++){
+    //
+    
+    /*for (int i = 0; (i < sortedArray.size()-1) && (sortedArray[i] < minMax.second);i++){
         for (int j = sortedArray[i]+1; (j < sortedArray[i+1]) && (j < minMax.second); j++){
             numsBtwMinMax.push_back(j);
         }
+    }*/
+    for (int i = 0; i < *k; i ++){
+        cout << arr[i] << " ";
     }
-    return numsBtwMinMax;
+    cout << endl;
+    return arr;
 }
 
 void print_nums(vector <int> &array){
@@ -81,8 +104,11 @@ int main(){
 
     pair<int,int> minMax(array[0],array[array.size()-1]);
 
-    vector<int> numsBetwMinMax = find_nums_between_minmax(array,minMax);
-    
-    print_nums(numsBetwMinMax);
+    int * arr = find_nums_between_minmax(array,minMax);
+    //delete[] arr;
+    //print_nums(numsBetwMinMax);
     print_nums(array);
 }
+
+
+
